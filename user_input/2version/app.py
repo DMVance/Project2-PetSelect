e228c067121095c75594af0ccd24ac87011855d5
Jessica Bates <bates.jessica72@gmail.com>
2020-12-01 22:59:15 -0600

    reorganized, added flask app

---
+++ user_input/2version/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+
+from size import size_
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def userinput():
+    errors = ""
+    if request.method == "POST":
+        user_size = None
+        try:
+            user_size = request.form["user_size"]
+        except:
+            errors += "issue"
+        if user_size is not None:
+            result = size_(user_size)
+            return '''
+                <html>
+                    <body>
+                        <p>The result is {result}</p>
+                        <p><a href="/">Click here to calculate again</a>
+                    </body>
+                </html>
+            '''.format(result=result)
+    return """
+    <html>
+        <body>
+            {errors}
+            <p>What size dog do you prefer? <br>Small (less than 20 pounds) <br>Medium (between 21 and 60 pounds) <br>Large (more than 60 pounds)</p>
+            <form method = "post" action = ".">
+                <p><input name="user_size" /></p>
+                <p><input type = "submit" value = "submit size"/></p>
+                </form>
+            </body>
+        </html>
+        """.format(errors=errors)
+
+
+
+    
+
+if __name__ == "__main__":
+    app.run(debug = True)
