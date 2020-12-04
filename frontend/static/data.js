05ae2649a9952965be3f336be54cec0c462c8d28
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-12-03 18:37:23 -0600

    changes to index.html template

---
+++ frontend/static/data.js
@@ -0,0 +1,530 @@
+const large_traits = [{
+    "loyal": {
+      "id": "loyal",
+      "name": "loyal",
+      "value": "loyal",
+      "size": "large"
+    },
+    "independent": {
+      "id": "independent",
+      "name": "independent",
+      "value": "independent",
+      "size": "large"
+    },
+    "intelligent": {
+      "id": "intelligent",
+      "name": "intelligent",
+      "value": "intelligent",
+      "size": "large"
+    },
+    "brave": {
+      "id": "brave",
+      "name": "brave",
+      "value": "brave",
+      "size": "large"
+    },
+    "docile": {
+      "id": "docile",
+      "name": "docile",
+      "value": "docile",
+      "size": "large"
+    },
+    "alert": {
+      "id": "alert",
+      "name": "alert",
+      "value": "alert",
+      "size": "large"
+    },
+    "responsive": {
+      "id": "responsive",
+      "name": "responsive",
+      "value": "responsive",
+      "size": "large"
+    },
+    "dignified": {
+      "id": "dignified",
+      "name": "dignified",
+      "value": "dignified",
+      "size": "large"
+    },
+    "composed": {
+      "id": "composed",
+      "name": "composed",
+      "value": "composed",
+      "size": "large"
+    },
+    "friendly": {
+      "id": "friendly",
+      "name": "friendly",
+      "value": "friendly",
+      "size": "large"
+    },
+    "receptive": {
+      "id": "receptive",
+      "name": "receptive",
+      "value": "receptive",
+      "size": "large"
+    },
+    "faithful": {
+      "id": "faithful",
+      "name": "faithful",
+      "value": "faithful",
+      "size": "large"
+    },
+    "courageous": {
+      "id": "courageous",
+      "name": "courageous",
+      "value": "courageous",
+      "size": "large"
+    },
+    "loving": {
+      "id": "loving",
+      "name": "loving",
+      "value": "loving",
+      "size": "large"
+    },
+    "protective": {
+      "id": "protective",
+      "name": "protective",
+      "value": "protective",
+      "size": "large"
+    },
+    "trainable": {
+      "id": "trainable",
+      "name": "trainable",
+      "value": "trainable",
+      "size": "large"
+    },
+    "dutiful": {
+      "id": "dutiful",
+      "name": "dutiful",
+      "value": "dutiful",
+      "size": "large"
+    },
+    "responsible": {
+      "id": "responsible",
+      "name": "responsible",
+      "value": "responsible",
+      "size": "large"
+    },
+    "affectionate": {
+      "id": "affectionate",
+      "name": "affectionate",
+      "value": "affectionate",
+      "size": "large"
+    },
+    "devoted": {
+      "id": "devoted",
+      "name": "devoted",
+      "value": "devoted",
+      "size": "large"
+    },
+    "playful": {
+      "id": "playful",
+      "name": "playful",
+      "value": "playful",
+      "size": "large"
+    },
+    "assertive": {
+      "id": "assertive",
+      "name": "assertive",
+      "value": "assertive",
+      "size": "large"
+    },
+    "energetic": {
+      "id": "energetic",
+      "name": "energetic",
+      "value": "energetic",
+      "size": "large"
+    },
+    "gentle": {
+      "id": "gentle",
+      "name": "gentle",
+      "value": "gentle",
+      "size": "large"
+    },
+    "confident": {
+      "id": "confident",
+      "name": "confident",
+      "value": "confident",
+      "size": "large"
+    },
+    "dominant": {
+      "id": "dominant",
+      "name": "dominant",
+      "value": "dominant",
+      "size": "large"
+    },
+    "strong willed": {
+      "id": "strong willed",
+      "name": "strong willed",
+      "value": "strong willed",
+      "size": "large"
+    },
+    "stubborn": {
+      "id": "stubborn",
+      "name": "stubborn",
+      "value": "stubborn",
+      "size": "large"
+    },
+    "clownish": {
+      "id": "clownish",
+      "name": "clownish",
+      "value": "clownish",
+      "size": "large"
+    },
+    "obedient": {
+      "id": "obedient",
+      "name": "obedient",
+      "value": "obedient",
+      "size": "large"
+    },
+    "kind": {
+      "id": "kind",
+      "name": "kind",
+      "value": "kind",
+      "size": "large"
+    },
+    "sweet-tempered": {
+      "id": "sweet-tempered",
+      "name": "sweet-tempered",
+      "value": "sweet-tempered",
+      "size": "large"
+    },
+    "steady": {
+      "id": "steady",
+      "name": "steady",
+      "value": "steady",
+      "size": "large"
+    },
+    "bold": {
+      "id": "bold",
+      "name": "bold",
+      "value": "bold",
+      "size": "large"
+    },
+    "proud": {
+      "id": "proud",
+      "name": "proud",
+      "value": "proud",
+      "size": "large"
+    },
+    "fearless": {
+      "id": "fearless",
+      "name": "fearless",
+      "value": "fearless",
+      "size": "large"
+    },
+    "calm": {
+      "id": "calm",
+      "name": "calm",
+      "value": "calm",
+      "size": "large"
+    },
+    "watchful": {
+      "id": "watchful",
+      "name": "watchful",
+      "value": "watchful",
+      "size": "large"
+    },
+    "hard-working": {
+      "id": "hard-working",
+      "name": "hard-working",
+      "value": "hard-working",
+      "size": "large"
+    },
+    "active": {
+      "id": "active",
+      "name": "active",
+      "value": "active",
+      "size": "large"
+    },
+    "easygoing": {
+      "id": "easygoing",
+      "name": "easygoing",
+      "value": "easygoing",
+      "size": "large"
+    },
+    "adaptable": {
+      "id": "adaptable",
+      "name": "adaptable",
+      "value": "adaptable",
+      "size": "large"
+    },
+    "trusting": {
+      "id": "trusting",
+      "name": "trusting",
+      "value": "trusting",
+      "size": "large"
+    },
+    "even tempered": {
+      "id": "even tempered",
+      "name": "even tempered",
+      "value": "even tempered",
+      "size": "large"
+    },
+    "lovable": {
+      "id": "lovable",
+      "name": "lovable",
+      "value": "lovable",
+      "size": "large"
+    },
+    "territorial": {
+      "id": "territorial",
+      "name": "territorial",
+      "value": "territorial",
+      "size": "large"
+    },
+    "familial": {
+      "id": "familial",
+      "name": "familial",
+      "value": "familial",
+      "size": "large"
+    },
+    "rational": {
+      "id": "rational",
+      "name": "rational",
+      "value": "rational",
+      "size": "large"
+    },
+    "cheerful": {
+      "id": "cheerful",
+      "name": "cheerful",
+      "value": "cheerful",
+      "size": "large"
+    },
+    "bright": {
+      "id": "bright",
+      "name": "bright",
+      "value": "bright",
+      "size": "large"
+    },
+    "companionable": {
+      "id": "companionable",
+      "name": "companionable",
+      "value": "companionable",
+      "size": "large"
+    },
+    "keen": {
+      "id": "keen",
+      "name": "keen",
+      "value": "keen",
+      "size": "large"
+    },
+    "reliable": {
+      "id": "reliable",
+      "name": "reliable",
+      "value": "reliable",
+      "size": "large"
+    },
+    "reserved": {
+      "id": "reserved",
+      "name": "reserved",
+      "value": "reserved",
+      "size": "large"
+    },
+    "powerful": {
+      "id": "powerful",
+      "name": "powerful",
+      "value": "powerful",
+      "size": "large"
+    },
+    "stable": {
+      "id": "stable",
+      "name": "stable",
+      "value": "stable",
+      "size": "large"
+    },
+    "quiet": {
+      "id": "quiet",
+      "name": "quiet",
+      "value": "quiet",
+      "size": "large"
+    },
+    "inquisitive": {
+      "id": "inquisitive",
+      "name": "inquisitive",
+      "value": "inquisitive",
+      "size": "large"
+    },
+    "quick": {
+      "id": "quick",
+      "name": "quick",
+      "value": "quick",
+      "size": "large"
+    },
+    "strong": {
+      "id": "strong",
+      "name": "strong",
+      "value": "strong",
+      "size": "large"
+    },
+    "happy": {
+      "id": "happy",
+      "name": "happy",
+      "value": "happy",
+      "size": "large"
+    },
+    "great-hearted": {
+      "id": "great-hearted",
+      "name": "great-hearted",
+      "value": "great-hearted",
+      "size": "large"
+    },
+    "tolerant": {
+      "id": "tolerant",
+      "name": "tolerant",
+      "value": "tolerant",
+      "size": "large"
+    },
+    "mischievous": {
+      "id": "mischievous",
+      "name": "mischievous",
+      "value": "mischievous",
+      "size": "large"
+    },
+    "eager": {
+      "id": "eager",
+      "name": "eager",
+      "value": "eager",
+      "size": "large"
+    },
+    "people-oriented": {
+      "id": "people-oriented",
+      "name": "people-oriented",
+      "value": "people-oriented",
+      "size": "large"
+    },
+    "curious": {
+      "id": "curious",
+      "name": "curious",
+      "value": "curious",
+      "size": "large"
+    },
+    "trustworthy": {
+      "id": "trustworthy",
+      "name": "trustworthy",
+      "value": "trustworthy",
+      "size": "large"
+    },
+    "gay": {
+      "id": "gay",
+      "name": "gay",
+      "value": "gay",
+      "size": "large"
+    },
+    "patient": {
+      "id": "patient",
+      "name": "patient",
+      "value": "patient",
+      "size": "large"
+    },
+    "athletic": {
+      "id": "athletic",
+      "name": "athletic",
+      "value": "athletic",
+      "size": "large"
+    },
+    "thoughtful": {
+      "id": "thoughtful",
+      "name": "thoughtful",
+      "value": "thoughtful",
+      "size": "large"
+    },
+    "generous": {
+      "id": "generous",
+      "name": "generous",
+      "value": "generous",
+      "size": "large"
+    },
+    "outgoing": {
+      "id": "outgoing",
+      "name": "outgoing",
+      "value": "outgoing",
+      "size": "large"
+    },
+    "agile": {
+      "id": "agile",
+      "name": "agile",
+      "value": "agile",
+      "size": "large"
+    },
+    "sociable": {
+      "id": "sociable",
+      "name": "sociable",
+      "value": "sociable",
+      "size": "large"
+    },
+    "bubbly": {
+      "id": "bubbly",
+      "name": "bubbly",
+      "value": "bubbly",
+      "size": "large"
+    },
+    "suspicious": {
+      "id": "suspicious",
+      "name": "suspicious",
+      "value": "suspicious",
+      "size": "large"
+    },
+    "unflappable": {
+      "id": "unflappable",
+      "name": "unflappable",
+      "value": "unflappable",
+      "size": "large"
+    },
+    "sensitive": {
+      "id": "sensitive",
+      "name": "sensitive",
+      "value": "sensitive",
+      "size": "large"
+    },
+    "good-natured": {
+      "id": "good-natured",
+      "name": "good-natured",
+      "value": "good-natured",
+      "size": "large"
+    },
+    "self-assured": {
+      "id": "self-assured",
+      "name": "self-assured",
+      "value": "self-assured",
+      "size": "large"
+    },
+    "lively": {
+      "id": "lively",
+      "name": "lively",
+      "value": "lively",
+      "size": "large"
+    },
+    "tenacious": {
+      "id": "tenacious",
+      "name": "tenacious",
+      "value": "tenacious",
+      "size": "large"
+    },
+    "aloof": {
+      "id": "aloof",
+      "name": "aloof",
+      "value": "aloof",
+      "size": "large"
+    },
+    "clever": {
+      "id": "clever",
+      "name": "clever",
+      "value": "clever",
+      "size": "large"
+    },
+    "fast": {
+      "id": "fast",
+      "name": "fast",
+      "value": "fast",
+      "size": "large"
+    },
+    "self-confidence": {
+      "id": "self-confidence",
+      "name": "self-confidence",
+      "value": "self-confidence",
+      "size": "large"
+    }
+  }]
