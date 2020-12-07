da5c7652ec10ba086a1a608c90ec8235b8549cab
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-12-07 17:40:18 -0600

    added rachel's code for appending checkboxes

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,24 +1,29 @@
-console.log("app.js is up")
+console.log("hello")
+
 
 function loadChar() {
+​
+    charList = d3.select(".characteristics")
+​
+    traitsList = Object.keys(large_traits[0])
 
-    Object.entries(large_traits[0]).forEach((trait, traitDict) => {
-        charList = d3.select("characteristics")
-        charList.append("li")
+
+​
+    traitsList.forEach(trait => {
+        console.log(trait)
+        listItem = charList.append("li")
             .classed("char-columns", true)
-        inputTag = charList.append("input")
+        
+        listItem.append("input")
             .attr("type", "checkbox")
-            .attr("id", traitDict.id)
-            .attr("name", traitDict.name)
-            .attr("value", traitDict.value)
-        inputTag.append("label")
-            .attr("for", traitDict.name)
-            .text(traitDict.name)
+            .attr("id", trait)
+            .attr("name", trait)
+            .attr("value", trait)
+            .attr("margin-right", "5px")
+​
+        listItem.append("label")
+            .attr("for", trait)
+            // .attr("padding-left", ".4em")
+            .text("  " + trait)
     })
 }
-
-
-
-
-// do i need to include an innerHTML statement to actually append to the html? 
-
