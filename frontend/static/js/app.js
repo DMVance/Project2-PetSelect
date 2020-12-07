334d6c16f516fe659ccbc7822a9220f900a9a0c4
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-12-07 11:59:57 -0600

    closer on app.js. no error, but still not appending

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,19 +1,24 @@
 console.log("app.js is up")
 
-large_traits[0].forEach((trait, traitDict) => {
-    charList = d3.select("characteristics")
-    console.log("app.js is up")
-    charList.append("li")
-        .classed("char-columns", true)
-    inputTag = charList.append("input")
-        .attr("type", "checkbox")
-        .attr("id", traitDict.id)
-        .attr("name", traitDict.name)
-        .attr("value", traitDict.value)
-    inputTag.append("label")
-        .attr("for", traitDict.name)
-        .text(traitDict.name)
-})
+function loadChar() {
+
+    Object.entries(large_traits[0]).forEach((trait, traitDict) => {
+        charList = d3.select("characteristics")
+        charList.append("li")
+            .classed("char-columns", true)
+        inputTag = charList.append("input")
+            .attr("type", "checkbox")
+            .attr("id", traitDict.id)
+            .attr("name", traitDict.name)
+            .attr("value", traitDict.value)
+        inputTag.append("label")
+            .attr("for", traitDict.name)
+            .text(traitDict.name)
+    })
+}
 
 
+
+
+// do i need to include an innerHTML statement to actually append to the html? 
 
