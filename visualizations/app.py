dc7d7f1961e87888286982031a5520cf5c0093c7
DMVance <csljrrt@gmail.com>
2020-12-08 00:21:20 -0600

    modified app.py

--- visualizations/app.py
+++ visualizations/app.py
@@ -6,15 +6,15 @@ from mongo_query import combined_queries
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route("/visualizations")
 def home():
     return render_template("index.html")
 
-@app.route("/data")
-def data():
-    with open(os.path.join("dogs.json")) as file:
-        data = json.load(file)
-    return data
+# @app.route("/data")
+# def data():
+#     with open(os.path.join("dogs.json")) as file:
+#         data = json.load(file)
+#     return data
 
 if __name__ == "__main__":
     app.run(debug=True)
