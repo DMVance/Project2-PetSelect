0a12fa22a1ad9f2df566e6cf081964700f313a75
Reid Haynie <reidhaynie@Reids-MacBook-Pro-2.local>
2020-12-07 18:03:55 -0600

    closer on styling of checkboxes

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,26 +1,32 @@
-console.log("hello")
+console.log("app.js is up")
+
+
+let selected_size
+let options = d3.select("#size").selectAll("option")
+options.each(function (d, i) {
+	let current_option = d3.select(this)
+	if (current_option.property("selected")) {
+		selected_size = current_option._groups[0][0].value
+	}
+})
+console.log(selected_size)
 
 
 function loadChar() {
-​
+
     charList = d3.select(".characteristics")
-​
+    
     traitsList = Object.keys(large_traits[0])
-
-
-​
     traitsList.forEach(trait => {
         console.log(trait)
         listItem = charList.append("li")
             .classed("char-columns", true)
-        
         listItem.append("input")
             .attr("type", "checkbox")
             .attr("id", trait)
             .attr("name", trait)
             .attr("value", trait)
-            .attr("margin-right", "5px")
-​
+            // .attr("margin-left", "5px")
         listItem.append("label")
             .attr("for", trait)
             // .attr("padding-left", ".4em")
