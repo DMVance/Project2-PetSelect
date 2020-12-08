f4c55682436d6df5986187d1b11b6c89fed62834
DMVance <csljrrt@gmail.com>
2020-12-08 00:19:55 -0600

    flask routes for visualizations

---
+++ visualizations/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+import os, json
+from mongo_query import combined_queries
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    with open(os.path.join("dogs.json")) as file:
+        data = json.load(file)
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
