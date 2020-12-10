3c93b2c70204c1c01222ca15c4fe8c21f23adabe
RachelLeeReynolds85 <rachelleereynolds85@gmail.com>
2020-12-10 11:08:20 -0600

    fixes breed results & streamlines characteristics populator

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,14 @@
 ## from [] import []
 ## from [] import []
 import os
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, make_response
 from flask_pymongo import PyMongo
+import requests
+import json
+import pandas as pd
+
+from breed_finder import best_breed
+
 
 app = Flask(__name__)
 
@@ -15,6 +21,34 @@ def index():
 def input():
     # jessica's python
     return render_template("input.html")
+
+@app.route("/findapup/create-entry", methods=["POST", "GET"])
+def create_entry():
+    adj_list = []
+    dog_size = []
+
+    req = request.get_json()    
+
+    adj_list = req["traits"]
+
+    for k, v in req.items():
+        if k =='size':
+            dog_size.append(v)
+
+    print(f"Dog size: {dog_size}; Adj: {adj_list}")
+    
+    # return req
+
+    breed = best_breed(adj_list, dog_size)
+    breeds = breed["name"]
+    
+    best_pup = []
+    best_pup.append(breeds[0])
+    best_pup.append(breeds[1])
+    best_pup.append(breeds[2])
+    print(best_pup)
+        
+    return breeds
     
 @app.route("/results")
 def results():
---
+++ frontend/breed_finder.py
@@ -0,0 +1,105 @@
+import requests
+import pandas as pd 
+import json
+from flask import jsonify
+
+# dog_size = ['medium']
+# adj_list = ['dutiful']
+
+
+def best_breed(adj_list, dog_size):
+
+    # Get API data
+    r = requests.get("https://api.thedogapi.com/v1/breeds")
+
+    data = r.json()
+    df1 = pd.DataFrame(data)
+    df2 = df1[["name","temperament", "weight"]]
+
+    # Explode out list of temperament adj
+    df3 = df2.assign(temperament=df2.temperament.str.split(',')).explode('temperament')
+
+    # Clean Data
+    df3["temperament"] = df3["temperament"].str.lower()
+    df3["temperament"] = df3["temperament"].str.strip()
+
+    # turn df into a dict 
+    dict_ = df3.to_dict('records')
+
+    for i in dict_:
+        i["weight"] = i["weight"]["imperial"]
+        i["points"] = 0
+        i['avg_weight'] = 0
+        i['size_category'] = ''
+
+
+    for i in dict_:
+        if i["weight"] == 'up - 18':
+            i["weight"] = '18 - 18'
+        else:
+            i["weight"] == i["weight"]
+
+    # get avg weights
+    for i in dict_:
+        if len(i["weight"]) == 5:
+            i['avg_weight'] = (int(i["weight"][4]) + int(i["weight"][0])) / 2
+        elif len(i["weight"]) == 6:
+            x =int(i["weight"][4] + i["weight"][5])
+            i['avg_weight'] = (x + int(i["weight"][0])) / 2
+        elif len(i["weight"]) == 7:
+            x =int(i["weight"][5] + i["weight"][6])
+            y =int(i["weight"][0] + i["weight"][1])
+            i['avg_weight'] = (x + y) / 2
+        elif len(i["weight"]) == 8:
+            x =int(i["weight"][5] + i["weight"][6] + i["weight"][7])
+            y =int(i["weight"][0] + i["weight"][1])
+            i['avg_weight'] = (x + y) / 2
+        elif len(i["weight"]) == 9:
+            x =int(i["weight"][6] + i["weight"][7] + i["weight"][8])
+            y =int(i["weight"][0] + i["weight"][1] + i["weight"][2])
+            i['avg_weight'] = (x + y) / 2
+
+
+    # categorize dog size based on avg weight
+    for i in dict_:
+        if i["avg_weight"] < 20:
+            i["size_category"] = 'small'
+        elif i["avg_weight"] < 60:
+            i["size_category"] = 'medium'
+        else:
+            i["size_category"] = 'large'
+
+
+
+
+    for c in adj_list:
+        for i in dict_:
+            for e in i:
+                for t in e:
+                    if c == [i][0]["temperament"]:
+                        [i][0]["points"] += 1
+
+
+    df4 = pd.DataFrame(dict_)
+
+    df4 = df4[df4["size_category"] == dog_size[0]]
+
+    # troubleshooting
+    print(df4["size_category"])
+
+    df4 = df4[["name", "points"]]
+
+    df5 = df4.groupby(by = ['name']).sum()
+
+    df6 = df5.sort_values("points", ascending = False)
+    df6 = df6.reset_index()
+    df6 = df6.head()
+
+    dict7 = df6.to_dict()
+
+    # final = json.dumps(dict7)
+
+    # print(dict7)
+    return dict7
+
+# best_breed(adj_list, dog_size)
