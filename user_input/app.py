35c1f0805d8c322d96c8fecef1294cf08a4712e5
Jessica Bates <bates.jessica72@gmail.com>
2020-12-10 20:49:13 -0600

    working on raw data

--- user_input/app.py
+++ user_input/app.py
@@ -1,24 +1,42 @@
-from flask import Flask
-from test_ideas_2 import size, choice, best_breed
-import requests
-import pandas as pd 
-
+from flask import jsonify, Flask
+# from test_ideas_2 import size, choice, best_breed
+# import requests
+# import pandas as pd 
+from print_raw_data import all_pups
+import pymongo
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "The app is up!"
+#     CONN="mongodb+srv://project-team:QA2yoZpRa9yZ3ZFV@cluster0.duyza.mongodb.net/austin_dogs?retryWrites=true&w=majority"
+#     client = pymongo.MongoClient(CONN)
+#     db = client.austin_dogs
 
-@app.route("/choice")
-def user_input():
-    size()
-    choice()
-    best_breed()
-    return best_breed()
+#     all_dogs_data = []
+#     for i in db.dogs.find():
+#         all_dogs_data.append({key: value for key, value in i.items() if not key == "_id"})
+
+#     # print(all_dogs_data)
+
+#     # dog_dict = all_dogs_data.to_dict()
+
+    return all_pups()
 
 
-    
+# @app.route("/choice")
+# def user_input():
+#     size()
+#     choice()
+#     best_breed()
+#     return best_breed()
+
+
+@app.route("/all")
+def all_dogs():
+    return "This works"
     
 
 if __name__ == "__main__":
